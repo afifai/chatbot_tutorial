95a3f0d36d0fee438f92e4b021d748652174d80e
Afif Akbar Iskandar <afifai@sci.ui.ac.id>
2020-09-10 21:36:57 +0700

    handle image

--- mybot.py
+++ mybot.py
@@ -1,6 +1,8 @@
 from helpers import kasus_hari_ini
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from random import randint
+from io import BytesIO
+
 def start(update, context):
     update.message.reply_text("Halo, Selamat Datang di Bot NgodingPython")
 
@@ -23,8 +25,17 @@ def tebakan(update, context):
     else:
         update.message.reply_text("Tebakan Kamu Benar")
 
+def save(update, context):
+    chat_id = str(update.update_id)
+    file_image = update.message.photo[0].get_file()
+    f = BytesIO(file_image.download_as_bytearray())
+    with open("images/"+chat_id+".jpg", "wb") as fi:
+        fi.write(f.getbuffer())
+    #update.message.reply_text("Foto Anda telah tersimpan")
+    update.message.reply_photo(open("images/"+chat_id+".jpg", "rb"))
+
 # MAIN PROGRAM
-TOKEN = '1385555541:AAFYQslJmw-yfROBTlY0JJJvbu-04wfGLcY'
+TOKEN = 'YOUR TOKEN'
 updater = Updater(TOKEN, use_context=True)
 
 angka = randint(1,10)
@@ -39,6 +50,7 @@ dp.add_handler(CommandHandler("kasus_hari_ini", kasushariini))
 
 # Menambah Message Handler
 dp.add_handler(MessageHandler(Filters.text, tebakan))
+dp.add_handler(MessageHandler(Filters.photo, save))
 
 # run bot
 updater.start_polling()
